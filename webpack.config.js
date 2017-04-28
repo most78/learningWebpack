9b40e9dc4c1ed09607133df56ab0d7fda9efd0d4
Marcos Ostos <marcos.ostos@gmail.com>
2017-04-28 19:56:09 +0200

    Composing Configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const merge = require ('webpack-merge');
+const parts = require('./webpack.parts');
 const PATHS = {
   app: path.join(__dirname, 'app'),
   build: path.join(__dirname, 'build'),
@@ -9,67 +11,37 @@ const PATHS = {
   // Entries have to resolve to files! They rely on Node
   // convention by default so if a directory contains *index.js*,
   // it resolves to that
-const commonConfig = {
-  entry: {
-    app: PATHS.app,
-  },
-  output: {
-    path: PATHS.build,
-    filename: '[name].js',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Learning Webpack',
-    }),
-  ],
-};
-
-const productionConfig = () => commonConfig;
-
-const developmentConfig = () => {
-  const config = {
-    devServer: {
-      //Enable history AOI fallback so HTML5 History API based
-     // routing works. Good for complex setups.
-      historyApiFallback: true,
-
-     // Display only erros to reduce te aount of output.
-      stats: 'errors-only',
-
-     // Parse host and port from env to allow customization.
-      host: process.env.HOST, // Defaults to 'localhost'
-      port: process.env.PORT, //Defaults to 8080
-      overlay: {
-        errors: true,
-        warningns: true,
-      },
-  },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          enforce: 'pre',
-
-          loader: 'eslint-loader',
-          options: {
-            emitWarning: true,
-          },
+const commonConfig = merge ([
+    {
+        entry: {
+         app:PATHS.app,
         },
-      ],
+        output: {
+            path: PATHS.build,
+            filename: '[name].js',
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                title: 'Learning webpack',
+            }),
+        ],
     },
-  };
+    parts.lintJavaScript({include: PATHS.app}),
+]);
 
-  return Object.assign(
-    {},
-    commonConfig,
-    config
-  );
-};
 
-module.exports = (env) => {
-  if (env === 'production'){
-    return productionConfig();
-  }
-  return developmentConfig();
+const productionConfig = () => merge([]);
+
+const developmentConfig = merge ([
+    parts.devServer({
+        host: process.env.HOST,
+        port: process.env.PORT,
+    }),
+]);
+
+module.exports = (env) =>{
+    if (env === 'prodcution'){
+        return merge(commonConfig, productionConfig);
+    }
+    return merge(commonConfig, developmentConfig);
 };
-//};
