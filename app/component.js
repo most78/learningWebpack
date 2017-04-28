15da8588d20189fecffcf4d0354b1e9e3a425256
Marcos Ostos <marcos.ostos@gmail.com>
2017-04-28 16:39:29 +0200

    Add WDS

--- app/component.js
+++ app/component.js
@@ -1,4 +1,4 @@
-export default (text = 'Hello world') => {
+export default (text = 'Hola mundo') => {
   const element = document.createElement('div');
   element.innerHTML = text;
   return element;
--- webpack.config.js
+++ webpack.config.js
@@ -5,10 +5,11 @@ const PATHS = {
   build: path.join(__dirname, 'build'),
 };
 
-module.exports = {
+//module.exports = {
   // Entries have to resolve to files! They rely on Node
   // convention by default so if a directory contains *index.js*,
   // it resolves to that
+const commonConfig = {
   entry: {
     app: PATHS.app,
   },
@@ -22,3 +23,36 @@ module.exports = {
     }),
   ],
 };
+
+const productionConfig = () => commonConfig;
+
+const developmentConfig = () => {
+  const config = {
+    devServer: {
+      //Enable history AOI fallback so HTML5 History API based
+     // routing works. Good for complex setups.
+     historyApiFallback: true,
+     
+     // Display only erros to reduce te aount of output.
+     stats: 'errors-only',
+     
+     // Parse host and port from env to allow customization.
+     host: process.env.HOST, // Defaults to 'localhost'
+     port: process.env.PORT, //Defaults to 8080
+    },
+  };
+  
+  return Object.assign(
+    {},
+    commonConfig,
+    config
+  );
+};
+
+module.exports = (env) => {
+  if (env === 'production'){
+    return productionConfig();
+  }
+  return developmentConfig();
+};
+//};
